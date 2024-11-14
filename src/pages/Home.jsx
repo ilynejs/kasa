788c2a815f6e675ee43627946f3b1bd781817110
rubendc <rubendcdev@proton.me>
2024-11-14 13:24:38 +0100

    Ajout des styles pour la page d'accueil et intégration des composants Banner et HotelCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
+import Banner from '../components/main/Banner';
+import HotelCard from '../components/main/HotelCard';
+import logements from '../data/logements.json';
+
 const Home = () => {
 	return (
 		<>
-			<h1>Home</h1>
+			<Banner page={'home'} />
+
+			<section className="home__hotel-cards">
+				{logements.map((hotel) => (
+					<HotelCard hotel={hotel} key={hotel.id} />
+				))}
+			</section>
 		</>
 	);
 };
