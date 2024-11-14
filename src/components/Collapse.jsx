a39771c33d49c91d740febfc77ec01a219782fd3
rubendc <rubendcdev@proton.me>
2024-11-14 17:10:47 +0100

    Ajout du composant Collapse avec styles associés pour une interface dynamique et réactive

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import icon from '@assets/icons/arrow.png';
+
+const Collapse = ({ title, content }) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		<>
+			<article
+				className={isOpen ? 'collapse collapse--open' : 'collapse'}
+			>
+				<header
+					className="collapse__header"
+					onClick={() => setIsOpen(!isOpen)}
+				>
+					<h2 className="collapse__header__title">{title}</h2>
+
+					<img
+						src={icon}
+						alt="arrow icon"
+						className={
+							isOpen
+								? 'collapse__header__icon--open'
+								: 'collapse__header__icon'
+						}
+					/>
+				</header>
+
+				<div
+					className={
+						isOpen
+							? 'collapse__content collapse__content--open'
+							: 'collapse__content'
+					}
+				>
+					{content.map((item, index) => (
+						<p key={index}>{item}</p>
+					))}
+				</div>
+			</article>
+		</>
+	);
+};
+
+export default Collapse;
