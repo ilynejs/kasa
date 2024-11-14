6ba6b0c827a82e9086caf9d0cf5d14a40bef73c2
rubendc <rubendcdev@proton.me>
2024-11-14 13:24:55 +0100

    Ajout du composant Banner à la page About pour un affichage dynamique

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,10 @@
+import Banner from '../components/main/Banner';
+
 const about = () => {
 	return (
 		<>
+			<Banner page={'about'} />
+
 			<h1>About</h1>
 			<p>This is the about page</p>
 		</>
