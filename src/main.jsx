d376fb60e96b1276cb3830bbff026d38a3bedb6e
rubendc <rubendcdev@proton.me>
2024-11-15 21:46:43 +0100

    Suppression du mode strict dans main.jsx et mise à jour de la configuration des routes dans AppRoutes.jsx pour améliorer la gestion des erreurs et la structure des routes.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import './assets/styles/index.scss';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import router from './routes/AppRoutes';
 import { RouterProvider } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<RouterProvider router={router} />
-	</StrictMode>
+	<RouterProvider router={router} />
 );
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -18,41 +18,53 @@ const apartmentLoader = async ({ params }) => {
 	return apartment;
 };
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <App />,
+			errorElement: <ErrorPage />,
+			children: [
+				{
+					path: '/',
+					element: <Home />,
+				},
+
+				{
+					path: '/about',
+					element: <About />,
+				},
+
+				{
+					path: '/apartment/:id',
+					element: <Apartment />,
+					loader: apartmentLoader,
+				},
+
+				{
+					'/error': {
+						element: <ErrorPage />,
+					},
+				},
+
+				{
+					'*': {
+						element: <ErrorPage />,
+					},
+				},
+			],
+		},
+	],
 	{
-		path: '/',
-		element: <App />,
-		errorElement: <ErrorPage />,
-		children: [
-			{
-				path: '/',
-				element: <Home />,
-			},
-
-			{
-				path: '/about',
-				element: <About />,
-			},
-
-			{
-				path: '/apartment/:id',
-				element: <Apartment />,
-				loader: apartmentLoader,
-			},
-
-			{
-				'/error': {
-					element: <ErrorPage />,
-				},
-			},
-
-			{
-				'*': {
-					element: <ErrorPage />,
-				},
-			},
-		],
-	},
-]);
+		future: {
+			v7_startTransition: true,
+			v7_relativeSplatPath: true,
+			v7_fetcherPersist: true,
+			v7_normalizeFormMethod: true,
+			v7_partialHydration: true,
+			v7_skipActionErrorRevalidation: true,
+		},
+	}
+);
 
 export default router;
