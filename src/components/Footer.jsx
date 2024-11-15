ef1f01cf7a99803f16b1813a6454a9809663840a
rubendc <rubendcdev@proton.me>
2024-11-16 00:49:46 +0100

    Refactorisation du composant Footer pour utiliser une importation dynamique du logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,9 @@
+import kasaLogo from '@assets/img/logo-white.png';
+
 const Footer = () => {
 	return (
 		<footer className="footer">
-			<img
-				src="/src/assets/img/logo-white.png"
-				alt="Logo Kasa"
-				className="footer__logo"
-			/>
+			<img src={kasaLogo} alt="Logo Kasa" className="footer__logo" />
 			<p className="footer__text">© 2020 Kasa. All rights reserved</p>
 		</footer>
 	);
