8a603a87f6a24cde102950146a8660596b166fe4
rubendc <rubendcdev@proton.me>
2024-11-15 21:09:38 +0100

    Remplacement de la page Hotel par Apartment avec styles associés

---
+++ src/pages/Apartment.jsx
@@ -0,0 +1,66 @@
+import { useParams, useNavigate } from 'react-router-dom';
+import apartments from '@data/apartments.json';
+import Slideshow from '@components/Slideshow';
+import Tag from '@components/Tag';
+import Host from '@components/Host';
+import Rating from '@components/Rating';
+import Collapse from '@components/Collapse';
+import { useEffect } from 'react';
+
+const Apartment = () => {
+	const { id } = useParams();
+	const navigate = useNavigate();
+	const apartment = apartments.find((apartment) => apartment.id === id);
+
+	useEffect(() => {
+		if (!apartment) {
+			navigate('/error');
+		}
+	}, [apartment, navigate]);
+
+	const rating = parseInt(apartment.rating);
+
+	return (
+		<>
+			<Slideshow
+				pictures={apartment.pictures}
+				imagesAlt={apartment.title}
+			/>
+
+			<section className="details">
+				<section className="details__left">
+					<h1 className="details__apartment-name">
+						{apartment.title}
+					</h1>
+					<p className="details__location">{apartment.location}</p>
+
+					<div className="details__tags">
+						{apartment.tags.map((tag, index) => (
+							<Tag tag={tag} key={index} />
+						))}
+					</div>
+				</section>
+
+				<section className="details__right">
+					<Host
+						name={apartment.host.name}
+						picture={apartment.host.picture}
+					/>
+
+					<Rating value={rating} />
+				</section>
+			</section>
+
+			<section className="apartment-informations">
+				<Collapse
+					title="Description"
+					content={Array(apartment.description)}
+				/>
+
+				<Collapse title="Équipements" content={apartment.equipments} />
+			</section>
+		</>
+	);
+};
+
+export default Apartment;
