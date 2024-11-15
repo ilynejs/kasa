88feabd534fe733d617a774d91ed68b4bddc095a
rubendc <rubendcdev@proton.me>
2024-11-15 21:05:41 +0100

    Renommage du composant HotelCard en ApartmentCard et ajout des styles associés.

---
+++ src/components/ApartmentCard.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+
+const Card = ({ apartment }) => {
+	return (
+		<Link to={`/apartment/${apartment.id}`} className="apartment-card">
+			<article className="apartment-card">
+				<div className="apartment-card__cover">
+					<img
+						src={apartment.cover}
+						alt={apartment.title}
+						className="apartment-card__cover__img"
+					/>
+				</div>
+
+				<div className="apartment-card__content">
+					<h2 className="apartment-card__title">{apartment.title}</h2>
+				</div>
+			</article>
+		</Link>
+	);
+};
+
+export default Card;
